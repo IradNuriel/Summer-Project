4d799f8f48f2c78eeeb5cf3f7d3df4a2c2409c6a
noaml13 <licht.noam@gmail.com>
2020-07-23 09:56:31 +0300

    adding comments to line builder (and fixing copying mistake)

--- summer-project/LineBuilder.cpp
+++ summer-project/LineBuilder.cpp
@@ -3,21 +3,19 @@
 
 // c'tor
 LineBuilder::LineBuilder(std::valarray<int> res, std::valarray<double> a) {
-	resolution = res;
+	resolution.resize(2);
+	std::copy(std::begin(res), std::end(res), std::begin(resolution));
 	angle = a / 180.0 * M_PI;
 }
 
 Line LineBuilder::getLine(Vec3d pos, std::valarray<int> pixel) const {
-	// casting int arrays into double arrays (to be used later)
+	// casting int array into double array (to be used later)
 	std::valarray<double> pixeld = std::valarray<double>(2);
 	std::copy(std::begin(pixel), std::end(pixel), std::begin(pixeld));
-	std::valarray<double> resd = std::valarray<double>(2);
-	std::copy(std::begin(resolution), std::end(resolution), std::begin(resd));
-
-	// IDK what this does (ask Licht)
-	pixeld = pixeld / (resd / 2.0);
-	pixeld[1] = -pixeld[1];
-	std::valarray<double> temp = (pixeld * tan(angle / 2.0)) / (resd / 2.0);
-	Vec3d v = { temp[0], temp[1], 1 };
+	
+	pixeld = pixeld - (resolution / 2); //normalizing pixel pos relative to center
+	pixeld[1] = -pixeld[1]; // coordinates (>x, ^y, .z)
+	std::valarray<double> temp = (pixeld * tan(angle / 2.0)) / (resolution / 2.0); // calculating x,y length relative to z (=1)
+	Vec3d v = { temp[0], temp[1], 1 }; //declaring the 3d line vector based on the calculation above
 	return Line(v, pos);
 }
--- summer-project/LineBuilder.h
+++ summer-project/LineBuilder.h
@@ -13,10 +13,10 @@
 // this entire class was translated from python, orig. code by Licht
 class LineBuilder {
 public:
-	LineBuilder(std::valarray<int> res = { 6000, 4000 }, std::valarray<double> a = { 66.276, 46.861 }); // c'tor
+	LineBuilder(std::valarray<double> res = { 6000, 4000 }, std::valarray<double> a = { 66.276, 46.861 }); // c'tor
 	Line getLine(Vec3d pos = Vec3d(), std::valarray<int> pixel = {0,0} ) const;
 protected:
-	std::valarray<int> resolution; // the camera's resolution (horizontal, vertical)
+	std::valarray<double> resolution; // the camera's resolution (horizontal, vertical)
 	std::valarray<double> angle; // the camera's angle of view (horisontal, vertical)
 };
 
