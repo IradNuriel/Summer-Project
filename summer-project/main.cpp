42d9473b0765bb785e64c67586408522ff72acc2
noaml13 <licht.noam@gmail.com>
2020-07-26 12:24:47 +0300

    const

--- summer-project/main.cpp
+++ summer-project/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	LineBuilder lb = LineBuilder();
-	Line l = lb.getLine({ 0,0,0 }, {100,500});
+	Line l = lb.getLine({ 0,0,0 }, {1000,500});
 	
 	std::cout << "Hello World!\n" << l.getV() << std::endl;
 }
