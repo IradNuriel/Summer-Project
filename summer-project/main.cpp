bd630add5c67e82883fceea8dfcef8fce04bcbe5
Irad Nuriel <irad9731@gmail.com>
2020-07-31 12:45:27 +0300

    added auto detection of features using opencv ORB detector. also added a test that requaire to #define _SET1_ and requaire the set1 photos to be in summer-project\set1

--- summer-project/main.cpp
+++ summer-project/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "LineBuilder.h"
 #include "Cluster.h"
+#include "ORBDetection.h"
 
+#define _SET1_
 
 Cluster getCluster(LineBuilder& lb, std::vector<std::valarray<int>>& pixel, int offset=2) {
 	Cluster clus = Cluster();
@@ -12,12 +14,11 @@ Cluster getCluster(LineBuilder& lb, std::vector<std::valarray<int>>& pixel, int 
 	return clus;
 }
 
-
-int main() {
+void checkLinePart() {
 	LineBuilder lb = LineBuilder();
-	std::vector<std::valarray<int>> pixel = { {5566, 1039}, { 4988,1032 }, { 4742,1021 }, { 4255, 1019 },{ 3747,1008 }, { 3544,1016 }};
-	std::vector<std::valarray<int>> topcircle = { {3900, 1180}, { 3329,1175 }, { 3056,1177 }, { 2571,1190 }, { 2062,1188 }, { 1880,1206 }};
-	std::vector<std::valarray<int>> power = { {4335, 2434}, { 3783,2428 }, { 3529,2437 }, { 3067,2446 }, { 2574,2448 }, { 2393,2454 }};
+	std::vector<std::valarray<int>> pixel = { {5566, 1039}, { 4988,1032 }, { 4742,1021 }, { 4255, 1019 },{ 3747,1008 }, { 3544,1016 } };
+	std::vector<std::valarray<int>> topcircle = { {3900, 1180}, { 3329,1175 }, { 3056,1177 }, { 2571,1190 }, { 2062,1188 }, { 1880,1206 } };
+	std::vector<std::valarray<int>> power = { {4335, 2434}, { 3783,2428 }, { 3529,2437 }, { 3067,2446 }, { 2574,2448 }, { 2393,2454 } };
 	std::vector<std::valarray<int>> desk = { {2269,2973},{1540,2977},{1123,2964},{511,2980} };
 	std::vector<std::valarray<int>> remoteControl = { {3838,3781},{3093,3782},{2608,3789},{1941,3796},{1258,3803},{911,3767} };
 	std::vector<std::valarray<int>> window = { {5784,2626},{5133,2635},{4473,2637},{4091,2667} }; //offset=0
@@ -26,5 +27,32 @@ int main() {
 	std::cout << clus << std::endl;
 	clus = getCluster(lb, power);
 	std::cout << clus << std::endl;
+}
+
+void checkORBDetectionPart() {
+	ORBDetection orbDetector;
+#ifdef _SET1_
+	for (int i = -2; i <= 3; i++) {
+		char filename[20];
+		sprintf_s(filename, "set1/%d.jpg", i);
+		cv::Mat src = cv::imread(cv::samples::findFile(filename), IMREAD_ANYCOLOR);
+		orbDetector.addImageToSet(src);
+	}
+	for (int i = 0; i < 6; i++) {
+		orbDetector.getFeaturesOfImage(i);
+		orbDetector.showImageWithKeyPoints(i);
+	}
+	for (int i = 5; i >= 0; i--) {
+		orbDetector.removeImageFromSet(i);
+	}
+#else
+	std::cout << "we need to test on set number 1!"<<std::endl;
+#endif
+}
+
+
+int main() {
+	checkLinePart();
+	checkORBDetectionPart();
 }
 
