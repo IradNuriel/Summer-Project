b65b6d11edbb87668654ae7e94cd1a5561286b0b
noaml13 <47696687+noaml13@users.noreply.github.com>
2020-08-10 16:36:04 +0300

    Update Matcher.h
    added back comments

--- summer-project/Matcher.h
+++ summer-project/Matcher.h
@@ -15,15 +15,28 @@ class Matcher {
 public:
 	Matcher();//defult c'tor
 	Matcher(const Matcher& other);//copy c'tor
-	void init_img(Image& img) const;
-	std::vector<pKeyPoint> match2(const cv::Mat& img1, const cv::Mat& img2, bool draw_match=false) const;//function addImageToSet, add img to the data set
-	std::vector<cv::DMatch> match2(Image& img1, Image& img2, bool draw_match=false) const;
-	void draw(const cv::Mat& img1, const cv::Mat& img2, const std::vector<pKeyPoint>& kvec) const;
-	void draw(const Image& img1, const Image& img2, const std::vector<cv::DMatch>& match, std::string title="Match") const;
-protected:
-	const int minHessian=1200;//number of keypoints
+	void init_img(Image& img) const; //function initImage, take an image object and initialize it with this->detector.
+	std::vector<pKeyPoint> match2(const cv::Mat& img1, const cv::Mat& img2, bool draw_match=false) const; /*function match2(const Mat&,const Mat&,bool), takes two image matrices as an input,
+														and return matching features that has been filtered by the Lowe's ratio test.*/
+	std::vector<cv::DMatch> match2(Image& img1, Image& img2, bool draw_match=false) const; /*function match2(Image&,Image&,bool),
+												takes two image objects as an input,
+												and return matching features that has been filtered by the Lowe's ratio test*/
+	void draw(const cv::Mat& img1, const cv::Mat& img2, const std::vector<pKeyPoint>& kvec) const; /*function draw(const Mat&,const Mat&,const vector<pKeyPoint>),
+													takes two image matrices as an input, and draw the matches between them*/
+	void draw(const Image& img1, const Image& img2, const std::vector<cv::DMatch>& match, std::string title="Match") const; /*function draw(const Image&,const Image&,const vector<pKeyPoint>,string="Match"),
+																  takes two images as an input, and draw the matches between them*/
+private:
+	const int minHessian = Constants::MIN_HESSIAN;//number of keypoints
 	cv::Ptr<cv::ORB> detector;//pointer to an ORB detector object
 	cv::Ptr<cv::DescriptorMatcher> matcher;
+};
+
+#endif
+
+protected:
+	const int minHessian=Constants::MIN_HESSIAN;//number of keypoints
+	cv::Ptr<cv::ORB> detector;//pointer to an ORB detector object
+	cv::Ptr<cv::DescriptorMatcher> matcher; //pointer to a DescriptorMatcher object
 };
 
 #endif
