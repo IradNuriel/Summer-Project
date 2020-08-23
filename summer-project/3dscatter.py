68daeda2f3be1e2c54ee4c6b29fee103df7c9d6c
noaml13 <47696687+noaml13@users.noreply.github.com>
2020-08-23 21:48:38 +0300

    3d scatter viewer (from file)

---
+++ summer-project/3dscatter.py
@@ -0,0 +1,32 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import argparse
+import functools
+
+
+
+
+def plot(points):
+    fig = plt.figure()
+    scatter = fig.add_subplot(111, projection='3d')
+
+    for point in points:
+        xs, zs, ys = point
+        scatter.scatter(xs, ys, zs, marker='o')
+
+    scatter.set_xlabel('X')
+    scatter.set_ylabel('Y')
+    scatter.set_zlabel('Z')
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='a script for 3d view of points')
+    parser.add_argument('--name', type=str, help='the file name which points you want to see', default="points.txt")
+
+    args = parser.parse_args()
+    separators = [',', '[', ']']
+    with open(args.name,"r") as fpoints:
+        points = [tuple(map(float, functools.reduce(lambda s, sep: s.replace(sep, ' '), separators, line).split()))
+                  for line in fpoints.read().split('\n') if line]
+        plot(points)
